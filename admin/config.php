436583737499c2036c1c4cf982081c3802f358db
sarkaromar <ausantemonir@gmial.com>
2017-08-24 19:45:00 +0600

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc21/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/oc21/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc21/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oc21/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/oc21/upload/admin/');
+define('DIR_SYSTEM', '/var/www/html/oc21/upload/system/');
+define('DIR_LANGUAGE', '/var/www/html/oc21/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/oc21/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/oc21/upload/system/config/');
+define('DIR_IMAGE', '/var/www/html/oc21/upload/image/');
+define('DIR_CACHE', '/var/www/html/oc21/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/oc21/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/oc21/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/oc21/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/oc21/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/oc21/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'dhaka%002');
+define('DB_DATABASE', 'oc21');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc21/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc21/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/oc21/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/html/oc21/upload/system/');
+define('DIR_LANGUAGE', '/var/www/html/oc21/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/oc21/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/oc21/upload/system/config/');
+define('DIR_IMAGE', '/var/www/html/oc21/upload/image/');
+define('DIR_CACHE', '/var/www/html/oc21/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/oc21/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/oc21/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/oc21/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/oc21/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'dhaka%002');
+define('DB_DATABASE', 'oc21');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
